aa4ee35c8f9f8469334a833750d28326a41417e2
guilhermeSCampos <guilhermeregular@gmail.com>
2023-05-14 17:29:04 -0300

    time set to sort at 00:00

--- src/server.js
+++ src/server.js
@@ -1,16 +1,18 @@
 const app = require('./app')
 const cron = require('node-cron');
+const moment = require('moment-timezone');
 const { sortClassicHeroes, sortQuoteHeroes, sortSkillHeroes } = require('./utils/sortHeroes')
 const { resetCount } = require('./utils/setStatus')
 
-cron.schedule('* * * * *', function () {
-  sortClassicHeroes();
-  sortQuoteHeroes();
-  sortSkillHeroes();
-  resetCount()
-  console.log('Sorteando herois a cada minuto');
-});
+moment.tz.setDefault('America/Sao_Paulo');
 
 app.listen(3000, () => {
+  cron.schedule('0 0 * * *', function() {
+    sortClassicHeroes();
+    sortQuoteHeroes();
+    sortSkillHeroes();
+    resetCount()
+    console.log('Sorteando herois as 00:00 UTC -3');
+  });
   console.log('API ON')
 })
