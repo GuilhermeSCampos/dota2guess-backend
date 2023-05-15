df6afb17896e51065db029ae3ac3aa32d20d1aac
guilhermeSCampos <guilhermeregular@gmail.com>
2023-05-15 04:02:45 -0300

    gmm

--- server.js
+++ server.js
@@ -1,23 +1,10 @@
 const app = require('./src/app')
 const cron = require('node-cron');
 const moment = require('moment-timezone');
-const { sortClassicHeroes, sortQuoteHeroes, sortSkillHeroes } = require('./src/utils/sortHeroes')
-const { resetCount } = require('./src/utils/setStatus')
+
 require('dotenv').config();
 
-const schedule = require('node-schedule');
-
 moment.tz.setDefault('America/Sao_Paulo');
-
-function sortAll() {
-  sortClassicHeroes();
-  sortQuoteHeroes();
-  sortSkillHeroes();
-  resetCount()
-  console.log('sorteou!')
-}
-
-schedule.scheduleJob('* * * * *', sortAll);
 
 app.listen(process.env.PORT || 9001, () => {
 
--- src/app.js
+++ src/app.js
@@ -1,9 +1,19 @@
 const heroesRouter = require('./routes/heroes.router')
 const statusRouter = require('./routes/status.router')
+const { sortClassicHeroes, sortQuoteHeroes, sortSkillHeroes } = require('./utils/sortHeroes')
+const { resetCount } = require('./utils/setStatus')
 const cors = require('cors')
 
 const express = require('express');
 require('express-async-errors')
+
+function sortAll() {
+  sortClassicHeroes();
+  sortQuoteHeroes();
+  sortSkillHeroes();
+  resetCount()
+  console.log('sorteou!')
+}
 
 const app = express();
 app.use(cors())
@@ -14,6 +24,11 @@ app.use('/status', statusRouter)
 app.get('/', (req_, res) => {
   res.send('API FUNCIONANDO!');
 });
+
+app.post('/sortAll', (req_, res) => {
+  sortAll();
+  res.send('Heróis Sorteados!')
+})
 
 
 module.exports = app
